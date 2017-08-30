351eb9cffdd9fb186a006a4165661308e06fc56c
itaouil <itaouil95@gmail.com>
2017-08-30 19:24:16 +0200

    Added data preprocessing part.

---
+++ multiple-linear-regression.py
@@ -0,0 +1,27 @@
+# Multiple Linear Regression
+
+""" Data preprocessing """
+
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing dataset and create matrix of features
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 4].values
+
+# Encode categorial values (state feature)
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder = LabelEncoder()
+X[:, 3] = labelencoder.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Avoid Dummy Variable Trap
+X = X[:, 1:]
+
+# Split dataset (training and test sets)
+from sklearn.model_selection import train_test_split
+X_train, Y_train, X_test, Y_test = train_test_split(X, Y, test_size = 1/5, random_state = 0)
